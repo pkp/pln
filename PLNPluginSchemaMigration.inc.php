85b83bf8e351b0728ce257ad120e324d7953e992
Alec Smecher <alec@smecher.bc.ca>
2021-04-14 16:56:00 -0700

    pkp/pkp-lib#6915 Use Schema facade

--- PLNPluginSchemaMigration.inc.php
+++ PLNPluginSchemaMigration.inc.php
@@ -14,7 +14,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Builder;
 use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Database\Capsule\Manager as Capsule;
+use Illuminate\Support\Facades\Schema;
 
 class PLNPluginSchemaMigration extends Migration {
 	/**
@@ -23,7 +23,7 @@ class PLNPluginSchemaMigration extends Migration {
 	 */
 	public function up() {
 		// PLN Deposit Objects
-		Capsule::schema()->create('pln_deposit_objects', function (Blueprint $table) {
+		Schema::create('pln_deposit_objects', function (Blueprint $table) {
 			$table->bigInteger('deposit_object_id')->autoIncrement();
 			$table->bigInteger('journal_id');
 			$table->bigInteger('object_id');
@@ -34,7 +34,7 @@ class PLNPluginSchemaMigration extends Migration {
 		});
 
 		// PLN Deposits
-		Capsule::schema()->create('pln_deposits', function (Blueprint $table) {
+		Schema::create('pln_deposits', function (Blueprint $table) {
 			$table->bigInteger('deposit_id')->autoIncrement();
 			$table->bigInteger('journal_id');
 			$table->string('uuid', 36)->nullable();
@@ -46,16 +46,16 @@ class PLNPluginSchemaMigration extends Migration {
 		});
 
 		// Temporarily back up the old scheduled_tasks entry for this plugin, if there is one
-		Capsule::connection()->unprepared("UPDATE scheduled_tasks SET class_name='plugins.generic.pln.classes.tasks.Depositor_TEMP' WHERE class_name='plugins.generic.pln.classes.tasks.Depositor'");
+		DB::unprepared("UPDATE scheduled_tasks SET class_name='plugins.generic.pln.classes.tasks.Depositor_TEMP' WHERE class_name='plugins.generic.pln.classes.tasks.Depositor'");
 		// Create a new scheduled_tasks entry for this plugin
-		Capsule::connection()->unprepared("INSERT INTO scheduled_tasks (class_name) VALUES ('plugins.generic.pln.classes.tasks.Depositor')");
+		DB::unprepared("INSERT INTO scheduled_tasks (class_name) VALUES ('plugins.generic.pln.classes.tasks.Depositor')");
 		// Update the last_run for the new entry from the old one, if it exists
-		switch (Capsule::connection()->getDriverName()) {
-			case 'mysql': Capsule::connection()->unprepared("UPDATE scheduled_tasks sta JOIN scheduled_tasks stb ON (stb.class_name='plugins.generic.pln.classes.tasks.Depositor_TEMP') SET sta.last_run = stb.last_run WHERE sta.class_name='plugins.generic.pln.classes.tasks.Depositor'"); break;
-			case 'pgsql': Capsule::connection()->unprepared("UPDATE scheduled_tasks sta SET last_run=stb.last_run FROM scheduled_tasks stb WHERE sta.class_name='plugins.generic.pln.classes.tasks.Depositor' AND stb.class_name='plugins.generic.pln.classes.tasks.Depositor_TEMP'"); break;
+		switch (DB::getDriverName()) {
+			case 'mysql': DB::unprepared("UPDATE scheduled_tasks sta JOIN scheduled_tasks stb ON (stb.class_name='plugins.generic.pln.classes.tasks.Depositor_TEMP') SET sta.last_run = stb.last_run WHERE sta.class_name='plugins.generic.pln.classes.tasks.Depositor'"); break;
+			case 'pgsql': DB::unprepared("UPDATE scheduled_tasks sta SET last_run=stb.last_run FROM scheduled_tasks stb WHERE sta.class_name='plugins.generic.pln.classes.tasks.Depositor' AND stb.class_name='plugins.generic.pln.classes.tasks.Depositor_TEMP'"); break;
 			default: throw new Exception('Unsupported database');
 		}
 		// Delete the old scheduled tasks entry
-		Capsule::connection()->unprepared("DELETE FROM scheduled_tasks WHERE class_name='plugins.generic.pln.classes.tasks.Depositor_TEMP'");
+		DB::unprepared("DELETE FROM scheduled_tasks WHERE class_name='plugins.generic.pln.classes.tasks.Depositor_TEMP'");
 	}
 }
