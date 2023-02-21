91dbb23cdac618b5f587758dedf27366682b744e
Jonas Raoni Soares da Silva <github@raoni.org>
2023-02-21 19:09:38 +0300

    #57 Dropped AppLocale calls

--- PLNPlugin.php
+++ PLNPlugin.php
@@ -24,7 +24,6 @@ use APP\core\PageRouter;
 use APP\notification\Notification;
 use APP\notification\NotificationManager;
 use APP\facades\Repo;
-use APP\i18n\AppLocale;
 use APP\plugins\generic\pln\classes\DepositDAO;
 use APP\plugins\generic\pln\classes\DepositObjectDAO;
 use APP\plugins\generic\pln\classes\form\PLNSettingsForm;
@@ -297,7 +296,6 @@ class PLNPlugin extends GenericPlugin {
 			if ($op) {
 				if (in_array($op, array('deposits'))) {
 					define('HANDLER_CLASS', 'PLNHandler');
-					AppLocale::requireComponents(LOCALE_COMPONENT_APP_COMMON);
 					$handlerFile =& $args[2];
 					$handlerFile = $this->getHandlerPath() . '/' . 'PLNHandler.php';
 				}
@@ -313,7 +311,6 @@ class PLNPlugin extends GenericPlugin {
 		switch($request->getUserVar('verb')) {
 			case 'settings':
 				$context = $request->getContext();
-				AppLocale::requireComponents(LOCALE_COMPONENT_APP_COMMON,  LOCALE_COMPONENT_PKP_MANAGER);
 				$form = new PLNSettingsForm($this, $context->getId());
 
 				if ($request->getUserVar('refresh')) {
@@ -343,7 +340,6 @@ class PLNPlugin extends GenericPlugin {
 				$depositDao = DAORegistry::getDAO('DepositDAO');
 
 				$context = $request->getContext();
-				AppLocale::requireComponents(LOCALE_COMPONENT_APP_COMMON,  LOCALE_COMPONENT_PKP_MANAGER);
 				$form = new PLNStatusForm($this, $context->getId());
 
 				if ($request->getUserVar('reset')) {
