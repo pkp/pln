d8d19f2224854872baf4c36094bcbf806c9e5d2b
Jonas Raoni Soares da Silva <github@raoni.org>
2023-02-21 19:19:47 +0300

    #57 Replaced deprecated Application::getApplication()

--- PLNPlugin.php
+++ PLNPlugin.php
@@ -403,7 +403,7 @@ class PLNPlugin extends GenericPlugin {
 			[
 				'On-Behalf-Of' => $this->getSetting($contextId, 'journal_uuid'),
 				'Journal-URL' => $dispatcher->url($request, Application::ROUTE_PAGE, $context->getPath()),
-				'Accept-language' => $language,
+				'Accept-Language' => $language,
 			]
 		);
 
