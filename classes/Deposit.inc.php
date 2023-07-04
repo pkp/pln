7ab2f5144f321ce51b9addf4f11f216c94086c34
Jonas Raoni Soares da Silva <github@raoni.org>
2023-07-04 17:21:51 +0300

    #28 Removed workaround to decide whether to post/create or put/edit a deposit

--- classes/Deposit.inc.php
+++ classes/Deposit.inc.php
@@ -168,10 +168,7 @@ class Deposit extends DataObject {
 	 * Set new (blank) deposit status
 	 */
 	public function setNewStatus() {
-		$oldAgreement = $this->getLockssAgreementStatus();
 		$this->setStatus(PLN_PLUGIN_DEPOSIT_STATUS_NEW);
-		// The agreement is kept until the deposit reaches the "transferred" state, the purpose is only to flag it as an update
-		$this->setLockssAgreementStatus($oldAgreement);
 	}
 
 	/**
