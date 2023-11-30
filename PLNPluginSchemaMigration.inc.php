82bc036ca8f53e62425fa6ec998805df66a853d1
Jonas Raoni Soares da Silva <github@raoni.org>
2023-11-30 12:17:02 +0300

    #28 Kept placeholder file to avoid upgrade issues

---
+++ PLNPluginSchemaMigration.inc.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * @file PLNPluginSchemaMigration.inc.php
+ *
+ * Copyright (c) 2014-2023 Simon Fraser University
+ * Copyright (c) 2000-2023 John Willinsky
+ * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
+ *
+ * @class PLNPluginSchemaMigration
+ * @brief Describe database table structures.
+ */
+
+use Illuminate\Database\Migrations\Migration;
+
+class PLNPluginSchemaMigration extends Migration {
+	/**
+	 * Run the migrations.
+	 * @return void
+	 */
+	public function up() {
+	}
+
+	/**
+	 * Rollback the migrations.
+	 * @return void
+	 */
+	public function down() {
+	}
+}
