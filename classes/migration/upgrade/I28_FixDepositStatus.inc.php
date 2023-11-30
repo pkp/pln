f2a4b40bc002efaec5d19d611d00da1b7aebb895
Jonas Raoni Soares da Silva <github@raoni.org>
2023-11-30 11:15:49 +0300

    #28 Separated the upgrade files

---
+++ classes/migration/upgrade/I28_FixDepositStatus.inc.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * @file classes/migration/upgrade/I28_FixDepositStatus.inc.php
+ *
+ * Copyright (c) 2014-2022 Simon Fraser University
+ * Copyright (c) 2000-2022 John Willinsky
+ * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
+ *
+ * @class I28_FixDepositStatus
+ * @brief Adds new fields to manage the deposit status and resets the status for all deposits.
+ */
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use PKP\install\DowngradeNotSupportedException;
+
+class I28_FixDepositStatus extends Migration {
+	/**
+	 * Run the migrations.
+	 * @return void
+	 */
+	public function up() {
+		if (Schema::hasColumn('pln_deposits', 'date_preserved')) {
+			return;
+		}
+		Schema::table('pln_deposits', function (Blueprint $table) {
+			$table->datetime('date_preserved')->nullable();
+			$table->string('staging_state')->nullable();
+			$table->string('lockss_state')->nullable();
+		});
+		// Reset status
+		DB::table('pln_deposits')->update(['status', null]);
+	}
+
+	/**
+	 * Rollback the migrations.
+	 * @return void
+	 */
+	public function down() {
+		throw new DowngradeNotSupportedException();
+	}
+}
---
+++ classes/migration/upgrade/I35_FixMissingField.inc.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * @file classes/migration/upgrade/I35_FixMissingField.inc.php
+ *
+ * Copyright (c) 2014-2022 Simon Fraser University
+ * Copyright (c) 2000-2022 John Willinsky
+ * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
+ *
+ * @class I35_FixMissingField
+ * @brief Before the version 2.0.4.3, it's needed to check for a missing "export_deposit_error" field
+ */
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use PKP\install\DowngradeNotSupportedException;
+
+class I35_FixMissingField extends Migration {
+	/**
+	 * Run the migrations.
+	 * @return void
+	 */
+	public function up() {
+		if (Schema::hasColumn('pln_deposits', 'export_deposit_error')) {
+			return;
+		}
+		Schema::table('pln_deposits', function (Blueprint $table) {
+			$table->string('export_deposit_error', 1000)->nullable();
+		});
+	}
+
+	/**
+	 * Rollback the migrations.
+	 * @return void
+	 */
+	public function down() {
+		throw new DowngradeNotSupportedException();
+	}
+}
